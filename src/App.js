6ff5b5d1a22267851d3449eb594079e5a085649f
Emmie28 <61668381+Emmie28@users.noreply.github.com>
2022-06-03 23:06:09 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,186 @@
+import * as React from 'react';
+
+
+
+const List = ({list, onRemoveItem}) =>
+   (
+    <ul>
+        {list.map((item) =>
+          (
+            <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem}/>
+        ))}
+      </ul>
+  );
+
+
+const Item = ({item, onRemoveItem}) =>
+   {
+     
+    return(
+      <li>
+   <span>
+     <a href={item.url }> {item.title}</a> 
+   </span>
+   <span> {item.author}</span>
+   <span> {item.num_comments}</span>
+   <span>{item.points}</span>
+   <span>
+     <button type='button' onClick={() => onRemoveItem(item)}>
+       Dismiss
+     </button>
+   </span>
+ </li>
+    );   
+   }
+   
+ 
+
+ // Creating a reuseable react component.
+const InputWithLabel = ({ id, value, type = 'text', onInputChange, isFocused, children, }) => {
+  
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current){
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+  
+  return(
+  <>
+    <label htmlFor={id}>{children}</label>
+    &nbsp;
+    <input
+      ref={inputRef}
+      id={id}
+      type={type}
+      value={value}
+      autoFocus={isFocused}
+      onChange={onInputChange}
+      />
+  </>
+  );
+};
+
+
+  // Custom Hook 'useSemiPersistentState'
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem('search') || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
+const storiesReducer = (state, action) => {
+  switch (action.type) {
+    case 'STORIES_FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+
+    case 'STORIES_FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    
+    case 'STORIES_FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+
+    case 'REMOVE_STORY':
+      return {
+        ...state,
+        data: state.data.filter(
+          (story) => action.payload.objectID !== story.objectID
+        ),
+      };
+  default:
+    throw new Error();
+    
+  }
+};
+
+const App = () => {
+  // Fetch data from a third party api.
+  const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
+
+  const [searchTerm, setSearchTerm] = useSemiPersistentState(
+    'search',
+    'React'
+    );
+
+  
+  const [stories, dispatchStories] = React.useReducer(
+    storiesReducer,
+    { data: [], isLoading: false, isError: false } );
+
+  React.useEffect(() => {
+
+    if (!searchTerm) return;
+    dispatchStories({ type: 'STORIES_FETCH_INIT'});
+
+    fetch(`${API_ENDPOINT}`)
+      .then((response) => response.json())
+      .then((result) => {
+        dispatchStories({
+          type: 'STORIES:FETCH_SUCCESS',
+          payload: result.hits,
+        });
+      })
+    .catch(()=> dispatchStories({ type: 'STORIES_FETCH_FAILURE'}));
+  }, [searchTerm]);
+
+  
+  const handleRemoveStory = (item) => {
+    
+    dispatchStories({
+      type: 'REMOVE_STORY',
+      payload: item,
+    });
+  };
+
+  const handleSearch = (event)=>{
+    setSearchTerm(event.target.value);
+   
+  };
+
+  return (
+    <div>¨
+      <h1>My React Journey So Far!</h1>
+      <InputWithLabel
+        id='search'
+        value={searchTerm}
+        isFocused
+        onInputChange={handleSearch}
+      > 
+        <strong>Search:</strong>
+      </InputWithLabel>
+      
+      <hr/>
+
+      {stories.isError && <p>Something went wrong ...</p>}
+
+      {stories.isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <List list={stories.data} onRemoveItem={handleRemoveStory}/>
+      )}
+
+    </div>
+  );
+}
+
+export default App;
